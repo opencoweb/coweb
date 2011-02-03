ce3241ce0e83dff6af3be9e6bb575066472e1ddf
Peter Parente <pparent@us.ibm.com>
2011-02-03 09:59:29 -0500

    Remove extra period in doc copyright

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Open Cooperative Web Framework'
-copyright = u'2011 The Dojo Foundation. All Rights Reserved.'
+copyright = u'2011 The Dojo Foundation. All Rights Reserved'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
