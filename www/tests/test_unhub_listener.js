319d153f0532901303eaab0b37a4a778189b5edf
Peter Parente <pparent@us.ibm.com>
2011-02-28 07:22:02 -0500

    Outline for listener tests

---
+++ www/tests/test_unhub_listener.js
@@ -0,0 +1,67 @@
+//
+// Tests the UnamangedHubListener implementation of ListenerInterface.
+// 
+// Copyright (c) The Dojo Foundation 2011. All Rights Reserved.
+//
+/*global define module test raises deepEqual ok equal strictEqual*/
+define([
+    'coweb/collab/UnmanagedHubListener',
+    'org/OpenAjax',
+    'coweb/topics'
+], function(UnmanagedHubListener, OpenAjax, topics) {
+    test('start', 1, function() {
+        
+    });
+    
+    test('stop', 1, function() {
+        
+    });
+    
+    test('set site id', 1, function() {
+        
+    });
+    
+    test('inbound sync', 1, function() {
+        
+    });
+    
+    test('outbound sync', 1, function() {
+        
+    });
+    
+    test('inbound notice', 1, function() {
+        
+    });
+    
+    test('inbound state request', 1, function() {
+        
+    });
+    
+    test('outbound state response', 1, function() {
+        
+    });
+    
+    test('inbound state response', 1, function() {
+        
+    });
+    
+    test('outbound service subscribe', 1, function() {
+        
+    });
+
+    test('outbound service unsubscribe', 1, function() {
+        
+    });
+
+    test('outbound service request', 1, function() {
+        
+    });
+
+    test('outbound engine sync', 1, function() {
+        
+    });
+
+    test('inbound engine sync', 1, function() {
+        
+    });
+});
