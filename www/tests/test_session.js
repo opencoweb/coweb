5397e16af2c6f6b5c283aff352181ff01eca55cb
Peter Parente <pparent@us.ibm.com>
2011-03-01 15:17:26 -0500

    Re-add wait for disconnect in abort test

--- www/tests/test_session.js
+++ www/tests/test_session.js
@@ -447,6 +447,7 @@ define([
 
     test('abort after updating', 1, function() {
         var self = this;
+        this.waitDisconnect = true;
     
         // check notification of leaving conference
         this.collabs[0].subscribeConferenceEnd(function(params) {
