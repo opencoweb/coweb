efc810e6874f5655ef870185472ce396fb7c7d4c
Peter Parente <pparent@us.ibm.com>
2011-05-10 12:06:30 -0400

    Version bump for 0.6

---
+++ js/release/coweb-0.6/coweb/session/bayeux/CowebExtension.js
@@ -0,0 +1,1 @@
+define(function(){var a=function(a){this._cometd=null,this._sessionid=a.sessionid};a.prototype.registered=function(a,b){this._cometd=b},a.prototype.unregistered=function(a,b){this._cometd=null},a.prototype.outgoing=function(a){var b=a.ext=a.ext||{},c=a.ext.coweb=a.ext.coweb||{};c.sessionid=this._sessionid;return a};return a})
