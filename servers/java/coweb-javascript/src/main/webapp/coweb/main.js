3da0f0aaf7931eb5e7c2073bcb979931d8203712
Chris Cotter <ccotter@utexas.edu>
2012-10-12 15:19:41 -0500

    Cleanup of /js and update one last version to 0.8.4-SNAPSHOT
    servers/java/coweb-admin/admin-ejb/pom.xml needed a version to be
    bumped.
    
    For #73 and in anticipation of new python enhancements #203, I have
    removed js/lib since the JS code now lives in
    servers/java/coweb-javascript. Other outdated scripts have been removed
    and I did some other general house cleaning.

--- servers/java/coweb-javascript/src/main/webapp/coweb/main.js
+++ servers/java/coweb-javascript/src/main/webapp/coweb/main.js
@@ -45,7 +45,7 @@ define([
 
     // factory interface
     return {
-        VERSION : '0.8.3.1',
+        VERSION : '0.8.4-SNAPSHOT',
 
         /**
          * Get the singleton cowebConfig.sessionImpl implementation of 
